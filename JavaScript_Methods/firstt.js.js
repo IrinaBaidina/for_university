f7f074fd28de290aea84720bc41733041a5eb442
Irina <baidinairina@list.rugit config --global user.email baidinairina@list.ru>
2022-04-08 18:31:05 +0300

    08.04.2022

---
+++ JavaScript_Methods/firstt.js.js
@@ -0,0 +1,46 @@
+//Задание 1
+var commands=[
+    {id: "1-command", command: "go"},
+    {id: "2-command", command: "rotate"},
+    {id: "3-command", command: "push"},
+    {id: "4-command", command: "pop"},
+    {id: "5-command", command: "reverse"}
+];
+
+var Queue = {
+    k: 6,
+    commands:[
+        {id: "1-command", command: "go"},
+        {id: "2-command", command: "rotate"},
+        {id: "3-command", command: "push"},
+        {id: "4-command", command: "pop"},
+        {id: "5-command", command: "reverse"}
+    ],
+    get property(){
+        k--;
+        var i = -1;
+        for (var j of commands) {
+            i++;
+            j.id = i+"-command";
+        }
+        function callBack(element){
+            return element.id==="0-command";
+        }
+        var element = commands.find(callBack);
+        commands.shift();
+        return(element.command)
+    },
+    set property(command){
+        k=7;
+        var str = k + "-command";
+        commands.push({id: str, command: command});
+        k++;
+    },
+    showQueue:function () {
+        var i = 0;
+        for (var j of commands) {
+            i++;
+            console.log(i +"."+ j.command);
+        }
+    }
+};
---
+++ JavaScript_Methods/third.js
@@ -0,0 +1,31 @@
+//Задание 2
+var paragragraph = "Однажды я шел перед вечером мимо базарной площади в лесничество. " + "Оно помещалось на окраине городка над рекой. " + "Среди улицы мальчишки играли в футбол. " + "Громкоговоритель висел на телеграфном столбе. " + "Он неожиданно защелкал.";
+
+function dop(paragragraph, index){
+    var NewStr = paragragraph.split(".");
+    return NewStr[index];
+}
+function split(paragragraph){
+    var strLen = [0, 0, 0, 0, 0];
+    var strWor = ["", "", "", "", ""];
+    arr = [
+        {length: 0, words: ""},
+        {length: 0, words: ""},
+        {length: 0, words: ""},
+        {length: 0, words: ""},
+        {length: 0, words: ""}
+    ];
+    var NewStr = paragragraph.split(".");
+    for (var j = 0; j < 5; j++) {
+        strLen[j] = dop(paragragraph, j).trim().length;
+        strWor[j] = dop(paragragraph, j).trim()+".";
+    };
+    i = 0;
+    for (var j of arr){
+        j.length = strLen[i];
+        j.words = strWor[i];
+        i++;
+    }
+    return arr;
+}
+console.log(split(paragragraph));
